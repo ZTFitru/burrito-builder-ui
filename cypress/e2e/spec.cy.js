8e3a493c4e4df007622d74573cc0812e4ec5d170
Zelalem Fitru <zelalemtfitru@gmail.com>
2024-09-23 12:39:24 -0400

    Installed cypress, and created a fixture.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,14 @@
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+describe("Burrito Builder spec", () => {
+  beforeEach(()=> {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {
+      statusCode: 200,
+      fixtures: 'tacoBell'
+    }).as('tacoList')
+
+    cy.visit('http://localhost:3000/')
+  })
+
+  it("should display title when page loads", () => {
+    cy.get('h1').should('contain', 'Burrito Builder')
   });
 });
